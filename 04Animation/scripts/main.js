eb0f453e6d46de638cfb8b555fab3e971c09cf3e
tianyuqin1996 <108187985+tianyuqin1996@users.noreply.github.com>
2022-06-27 03:41:36 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -7,6 +7,10 @@ var myOtherBox = document.getElementById('myOtherBox');
 function spin(){
  	myOtherBox.object3D.rotation.y += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.y);
+ myOtherBox.object3D.rotation.x += rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.x);
+ myOtherBox.object3D.rotation.z += rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.z);
  }
 
  setInterval(spin, 16); //equivalent to 60 fps
