20b2ff7406a6d52645e25ca505a01520ad587f93
tianyuqin1996 <108187985+tianyuqin1996@users.noreply.github.com>
2022-06-27 04:34:20 +0200

    Update main.js

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -16,7 +16,10 @@ setInterval(spin, 16);
  	rotationSpeed = 0.01;
  	console.log('mouse enter');
 	 
-	  myOtherBox.addEventListener('mouseleave', function(){
+ });
+
+  myOtherBox.addEventListener('mouseleave', function(){
  	rotationSpeed = 0.001;
  	console.log('mouse leave');
- });
+	  
+  });
